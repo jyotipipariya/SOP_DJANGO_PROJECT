78d3538db38be833e8fa0a6603ae9254d4806853
jyotipipariya <jyotipipariya@gmail.com>
2023-01-14 15:08:12 +0530

    success

---
+++ sop_django/sop_django/ORS/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('',views.index),
+    path('auth/<page>/',views.auth),
+    path('<page>/', views.actionId),
+    path('<page>/<operation>/<int:id>',views.actionId),
+    path('<page>/',views.actionId),
+
+ ]
